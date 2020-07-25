85fed7dafed69e1deb88d2e822925be486fd797b
Numan Azad <namanajid54@gmail.com>
2020-07-26 00:12:23 +0530

    first commit

---
+++ mainn.js
@@ -0,0 +1,37 @@
+/*code to fade offset top document gradually*/
+$(document).ready(function(){
+  var doc=$(document);
+  var content=$(".fade-scroll");
+
+  doc.on("scroll",function(){
+    var scrolledpixel=doc.scrollTop();
+    
+    content.each(function(){
+      var allcontents=$(this);
+      var elementscrolled=allcontents.offset().top;
+      var unit=scrolledpixel-elementscrolled;
+      if(scrolledpixel>elementscrolled){
+        allcontents.css({"opacity":1-(unit)/400});
+      }
+    })
+  })
+});
+/*done*/
+
+/*code to SLIDE IN ELEMENTS*/
+var sliders=document.querySelectorAll(".slide-in");
+const appearoptions={threshold:.5, rootMargin:"0px 0px -100px 0px"};
+var appearonscroll=new IntersectionObserver(function(entries,appearonscroll){
+ entries.forEach(entry=>{
+   if(!entry.isIntersecting){
+     return;
+   } else{
+     entry.target.classList.add("appear");
+     appearonscroll.unobserve(entry.target);
+   }
+ });
+},appearoptions);
+
+sliders.forEach(slider=>{
+  appearonscroll.observe(slider);
+});
